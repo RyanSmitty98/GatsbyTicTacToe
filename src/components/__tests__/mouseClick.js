e11426c2ec5df5a3d61ad3f24333f4bee5cc397f
RyanSmitty98 <Ryan_Smith303@mymail.eku.edu>
2020-12-03 14:47:43 -0500

    still cant write tests

--- src/components/__tests__/mouseClick.js
+++ src/components/__tests__/mouseClick.js
@@ -1,13 +1,14 @@
 import React from "react";
-import {cleanup, fireEvent, render} from '@testing-library/react';
+import {cleanup, fireEvent, getAllByDisplayValue, queryAllByDisplayValue, render} from '@testing-library/react';
 import renderer from "react-test-renderer";
-import {shallow} from 'enzyme';
 import CheckboxWithLabel from '../CheckboxWithLabel';
 
 describe('Test Button component', () => {
     it('Test click event', () => {
       // when someone clicks in the first box it will test to see if an x is placed there
-      fireEvent.click(getByLabelText(/off/i));
+      fireEvent.click(getByValue(/ /i));
+
+      expect(queryByValue(/X/i)).toBeTruthy();
 
 
     });
