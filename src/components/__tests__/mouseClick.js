06ab4014b0177520840b413de0a5dafaf234e0a4
RyanSmitty98 <Ryan_Smith303@mymail.eku.edu>
2020-12-03 13:40:52 -0500

    added check test

--- src/components/__tests__/mouseClick.js
+++ src/components/__tests__/mouseClick.js
@@ -1,8 +1,14 @@
 import React from "react";
+import {cleanup, fireEvent, render} from '@testing-library/react';
 import renderer from "react-test-renderer";
+import {shallow} from 'enzyme';
+import CheckboxWithLabel from '../CheckboxWithLabel';
 
 describe('Test Button component', () => {
     it('Test click event', () => {
       // when someone clicks in the first box it will test to see if an x is placed there
+      fireEvent.click(getByLabelText(/off/i));
+
+
     });
   });
