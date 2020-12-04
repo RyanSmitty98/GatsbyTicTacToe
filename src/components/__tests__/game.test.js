b8dd3d2f5befc20330d67637097e5968c77ff346
RyanSmitty98 <Ryan_Smith303@mymail.eku.edu>
2020-12-04 12:11:58 -0500

    another test works

--- src/components/__tests__/game.test.js
+++ src/components/__tests__/game.test.js
@@ -3,7 +3,7 @@ import Game from '../Game';
 import {Enzyme, shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   shallow(<Game />);
