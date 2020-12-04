383f4295c3dd383880eb32dd9bcba58b75bfd5bd
RyanSmitty98 <Ryan_Smith303@mymail.eku.edu>
2020-12-04 12:42:24 -0500

    tests are working better

--- src/components/__tests__/board.test.js
+++ src/components/__tests__/board.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Board from '../Board';
-
-import { configure } from 'enzyme';
+import { shallow, configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
-  shallow(<Board />);
+    let squares = Array(9).fill(null)
+    shallow(<Board squares={squares}/>);
 });
+
+it('calls onClick event on click of a board square', () =>{
+    let squares = Array(9).fill(null)
+    const onClick = jest.fn();
+    let wrapper = mount(<Board squares={squares} onClick={onClick}/>);
+    wrapper.find('button.square').first().simulate('click');
+    expect(onClick).toBeCalledWith(0)
+  })
