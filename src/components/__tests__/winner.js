546872b6cca565749df77fa495e0c1a3c6a9a60f
RyanSmitty98 <Ryan_Smith303@mymail.eku.edu>
2020-12-04 18:28:52 -0500

    working on time reversal

--- src/components/__tests__/winner.js
+++ src/components/__tests__/winner.js
@@ -19,9 +19,102 @@ it('Show winner vertically', () => {
     //second player turn (place O in 2x1)
     const secondPlayer = wrapper.find('div.game-info').children().first().text()
     expect(secondPlayer).toEqual('Next player: O')
-    const turn2 = wrapper.find('button.square').second
+    const turn2 = wrapper.find('button.square').at(1)
     turn2.simulate('click')
     
+    //player 1 (1x2)
+    const turn3 = wrapper.find('button.square').at(4)
+    turn3.simulate('click')
+
+    //player 2 (2x2)
+    const turn4 = wrapper.find('button.square').at(5)
+    turn4.simulate('click')
+
+    //player 1 (1x3)
+    const turn5 = wrapper.find('button.square').at(8)
+    turn5.simulate('click')
+
     const winner = wrapper.find('div.game-info').children().first().text()
     expect(winner).toEqual('Winner: X')
-  })
+    })
+
+    it('Show winner horizontally', () => {
+        const wrapper = mount(<Game/>)
+    
+        //first player and their move(X in spot 1x1)
+        const button = wrapper.find('button.square').first()
+        button.simulate('click')
+    
+        //second player turn (place O in 1x2)
+        const turn2 = wrapper.find('button.square').at(3)
+        turn2.simulate('click')
+        
+        //player 1 (2x1)
+        const turn3 = wrapper.find('button.square').at(1)
+        turn3.simulate('click')
+    
+        //player 2 (3x3)
+        const turn4 = wrapper.find('button.square').at(4)
+        turn4.simulate('click')
+    
+        //player 1 (1x3)
+        const turn5 = wrapper.find('button.square').at(2)
+        turn5.simulate('click')
+    
+        const winner = wrapper.find('div.game-info').children().first().text()
+        expect(winner).toEqual('Winner: X')
+        })
+
+    it('Show winner horizontally', () => {
+        const wrapper = mount(<Game/>)
+    
+        //first player and their move(X in spot 1x1)
+        const button = wrapper.find('button.square').first()
+        button.simulate('click')
+    
+        //second player turn (place O in 1x2)
+        const turn2 = wrapper.find('button.square').at(3)
+        turn2.simulate('click')
+        
+        //player 1 (2x1)
+        const turn3 = wrapper.find('button.square').at(1)
+        turn3.simulate('click')
+    
+        //player 2 (3x3)
+        const turn4 = wrapper.find('button.square').at(4)
+        turn4.simulate('click')
+    
+        //player 1 (1x3)
+        const turn5 = wrapper.find('button.square').at(2)
+        turn5.simulate('click')
+    
+        const winner = wrapper.find('div.game-info').children().first().text()
+        expect(winner).toEqual('Winner: X')
+        })
+
+    it('Show winner diagonally', () => {
+        const wrapper = mount(<Game/>)
+        
+        //first player and their move(X in spot 1x1)
+        const button = wrapper.find('button.square').first()
+        button.simulate('click')
+        
+        //second player turn (place O in 1x2)
+        const turn2 = wrapper.find('button.square').at(3)
+        turn2.simulate('click')
+            
+        //player 1 (2x1)
+         const turn3 = wrapper.find('button.square').at(4)
+        turn3.simulate('click')
+        
+        //player 2 (3x3)
+        const turn4 = wrapper.find('button.square').at(6)
+        turn4.simulate('click')
+        
+        //player 1 (1x3)
+        const turn5 = wrapper.find('button.square').at(8)
+        turn5.simulate('click')
+        
+        const winner = wrapper.find('div.game-info').children().first().text()
+        expect(winner).toEqual('Winner: X')
+        })
