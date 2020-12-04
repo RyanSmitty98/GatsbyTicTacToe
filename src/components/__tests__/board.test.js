1acd7ecf1c7aaaa4e943d4fcd624909d0315461e
RyanSmitty98 <Ryan_Smith303@mymail.eku.edu>
2020-12-04 12:12:03 -0500

    another test works

--- src/components/__tests__/board.test.js
+++ src/components/__tests__/board.test.js
@@ -1,11 +1,11 @@
-//import React from 'react';
-//import Board from '../Board';
+import React from 'react';
+import Board from '../Board';
 
-//import {Enzyme, shallow, configure} from 'enzyme';
-//import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-//Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
-//it('renders without crashing', () => {
- // shallow(<Board />);
-//});
+it('renders without crashing', () => {
+  shallow(<Board />);
+});
