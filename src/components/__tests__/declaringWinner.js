80a53a89e2b39f4ff66b1595b90edf0a9547ed97
RyanSmitty98 <Ryan_Smith303@mymail.eku.edu>
2020-12-05 12:50:49 -0500

    changing final test to have O win instead

--- src/components/__tests__/declaringWinner.js
+++ src/components/__tests__/declaringWinner.js
@@ -60,33 +60,37 @@ it('Show winner horizontally', () => {
     expect(winner).toEqual('Winner: X')
     })
 
-it('Show winner vertically', () => {
+it('Show winner vertically, O wins', () => {
     const wrapper = mount(<Game/>)
 
     //first player and their move(X in spot 1x1)
     const firstPlayer = wrapper.find('div.game-info').children().first().text()
     expect(firstPlayer).toEqual('Next player: X')
-    const button = wrapper.find('button.square').first()
+    const button = wrapper.find('button.square').at(1)
     button.simulate('click')
 
     //second player turn (place O in 2x1)
     const secondPlayer = wrapper.find('div.game-info').children().first().text()
     expect(secondPlayer).toEqual('Next player: O')
-    const turn2 = wrapper.find('button.square').at(1)
+    const turn2 = wrapper.find('button.square').first()
     turn2.simulate('click')
     
     //player 1 (1x2)
-    const turn3 = wrapper.find('button.square').at(4)
+    const turn3 = wrapper.find('button.square').at(3)
     turn3.simulate('click')
 
     //player 2 (2x2)
-    const turn4 = wrapper.find('button.square').at(5)
+    const turn4 = wrapper.find('button.square').at(4)
     turn4.simulate('click')
 
     //player 1 (1x3)
-    const turn5 = wrapper.find('button.square').at(8)
+    const turn5 = wrapper.find('button.square').at(6)
     turn5.simulate('click')
 
+    //player 2 (2x2)
+    const turn6 = wrapper.find('button.square').at(8)
+    turn6.simulate('click')
+
     const winner = wrapper.find('div.game-info').children().first().text()
-    expect(winner).toEqual('Winner: X')
+    expect(winner).toEqual('Winner: O')
     })
